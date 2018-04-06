b3b927733dc83383829340fdfd0188a948eb71cc
Jonathan Selwall <web@selwall.se>
2018-04-06 18:17:21 +0200

    Added proper color variables and some variables for spacing

--- client/components/_settings/_variables/index.js
+++ client/components/_settings/_variables/index.js
@@ -1,11 +1,21 @@
-const variables = `
-  :root {
-    --purple: purple;
-    --green: green;
-    --light-grey: grey;
-    --white: white;
-    --black: black;
-  }
-`;
+export const color = {
+  purple: '#6100f8',
+  green: '#36d8b3',
+  grey: '#e3e8ef',
+  lightGrey: '#f9f9fa',
+  white: '#ffffff',
+  black: '#3b3b41',
+  yellow: '#fcb38a',
+  red: '#fd596b',
+};
 
-export default variables;
+export const spacing = {
+  xs: '10px',
+  sm: '15px',
+  md: '20px',
+  lg: '30px',
+};
+
+export const typography = {
+  purple: 'purple',
+};
